d7bdc362251a2b10aa16bc5007e3667f39581415
Cooper Simpson <cooper@coopersimpson.com>
2022-08-21 18:21:07 +1000

    Add calculator functions

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Add
+function add(num1, num2) {
+    return num1 + num2
+}
+
+// Subtract
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+// Multiply
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+// Divide
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+// Operate
+function operate(operator, num1, num2) {
+    return operator(num1, num2)
+}
