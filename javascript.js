50f420a223ad89357b04ec23463f874c51332823
Cooper Simpson <cooper@coopersimpson.com>
2022-08-27 09:03:47 +1000

    Add clear function and reset screen handling

--- javascript.js
+++ javascript.js
@@ -12,23 +12,47 @@ const deleteButton = document.getElementById('deleteBtn')
 const screenCurrent = document.getElementById('screen-current')
 const screenPrevious = document.getElementById('screen-previous')
 
+// window.addEventListener('keydown', handleKeyboardInput)
+// equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+// deleteButton.addEventListener('click', deleteNumber)
+// decimalButton.addEventListener('click', appendPoint)
 
 digitButtons.forEach((digit) => {
     digit.addEventListener('click', () => appendNumber(digit.dataset.number))
 })
-
 operatorButtons.forEach((operator) => {
-    operator.addEventListener('click', () => console(operator.dataset.operator))
+    operator.addEventListener('click', () => setOperation(operator.dataset.operator))
 })
 
+//////
 
+function resetScreen() {
+    screenCurrent.textContent = ''
+    shouldResetScreen = false
+}
 
 function appendNumber(number) {
+    if (screenCurrent.textContent === '0' || shouldResetScreen) {
+        resetScreen()
+    }
     screenCurrent.textContent += number
 }
 
 function setOperation(operator) {
- // needing to store first operand here
+    // if (currentOperation !== null) evaluate()
+    firstOperand = screenCurrent.textContent
+    currentOperation = operator
+    screenPrevious.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetScreen = true
+}
+
+function clear() {
+    screenCurrent.textContent = '0'
+    screenPrevious.textContent =''
+    firstOperand = ''
+    secondOperand = ''
+    currentOperation = null
 }
 
 
