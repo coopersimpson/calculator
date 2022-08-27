30eda2ac83e667502ca8a378a5b5e5993b50bd75
Cooper Simpson <cooper@coopersimpson.com>
2022-08-27 10:03:21 +1000

    Buttons working

--- javascript.js
+++ javascript.js
@@ -13,10 +13,10 @@ const screenCurrent = document.getElementById('screen-current')
 const screenPrevious = document.getElementById('screen-previous')
 
 // window.addEventListener('keydown', handleKeyboardInput)
-// equalsButton.addEventListener('click', evaluate)
+equalsButton.addEventListener('click', evaluate)
 clearButton.addEventListener('click', clear)
-// deleteButton.addEventListener('click', deleteNumber)
-// decimalButton.addEventListener('click', appendPoint)
+deleteButton.addEventListener('click', deleteNumber)
+decimalButton.addEventListener('click', appendPoint)
 
 digitButtons.forEach((digit) => {
     digit.addEventListener('click', () => appendNumber(digit.dataset.number))
@@ -25,7 +25,7 @@ operatorButtons.forEach((operator) => {
     operator.addEventListener('click', () => setOperation(operator.dataset.operator))
 })
 
-//////
+
 
 function resetScreen() {
     screenCurrent.textContent = ''
@@ -33,14 +33,13 @@ function resetScreen() {
 }
 
 function appendNumber(number) {
-    if (screenCurrent.textContent === '0' || shouldResetScreen) {
+    if (screenCurrent.textContent === '0' || shouldResetScreen)
         resetScreen()
-    }
     screenCurrent.textContent += number
 }
 
 function setOperation(operator) {
-    // if (currentOperation !== null) evaluate()
+    if (currentOperation !== null) evaluate()
     firstOperand = screenCurrent.textContent
     currentOperation = operator
     screenPrevious.textContent = `${firstOperand} ${currentOperation}`
@@ -55,7 +54,35 @@ function clear() {
     currentOperation = null
 }
 
+function appendPoint() {
+    if (shouldResetScreen) resetScreen()
+    if (screenCurrent.textContent === '')
+      screenCurrent.textContent = '0'
+    if (screenCurrent.textContent.includes('.')) return
+    screenCurrent.textContent += '.'
+  }
 
+function deleteNumber() {
+    screenCurrent.textContent = screenCurrent.textContent
+      .toString()
+      .slice(0, -1)
+  }
+  
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '/' && screenCurrent.textContent === '0') {
+        alert("You can't divide by 0!")
+        return
+    }
+    secondOperand = screenCurrent.textContent
+    screenCurrent.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand))
+    screenPrevious.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    currentOperation = null
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+  }
 
 function add(num1, num2) {
     return num1 + num2
@@ -73,6 +100,20 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-// function operate(operator, num1, num2) {
-//     return operator(num1, num2)
-// }
+function operate(operator, num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    switch (operator) {
+        case '+':
+            return add(num1, num2)
+        case '−':
+            return substract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '/':
+            if (num2 === 0) return null
+            else return divide(num1, num2)
+        default:
+            return null    
+    }
+}
